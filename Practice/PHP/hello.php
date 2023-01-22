13a7c75c816b8a74ecc1154d7b44e211d0cde78a
sudip paudel <paudelsudip134@gmail.com>
2023-01-22 20:03:23 +0545

    added some pratices

--- Practice/PHP/hello.php
+++ Practice/PHP/hello.php
@@ -1,4 +1,23 @@
 <?php
+//display
 
 echo "Hello!!!!";
+echo"</br>";
+//variable using $
+$name="sudip";
+
+echo"</br>";
+
+echo"This is $name";
+
+echo"</br>";
+echo 'this is $name';
+
+echo"</br>";
+//defining the constant
+define('$PI',3.14);
+echo"PI";
+echo"</br>";
+echo"</br>";
+echo"</br>";
 ?>
